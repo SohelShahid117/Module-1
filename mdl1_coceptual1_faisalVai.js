c5e849cea541a2c2c0e52778a47f5098aed253be
SohelShahid117 <sohelshahid09@gmail.com>
2023-09-24 20:56:46 +0600

    first commit

---
+++ mdl1_coceptual1_faisalVai.js
@@ -0,0 +1,56 @@
+ //30 min teke soro hbe
+//vaier ajkr cls er 95t JS file clct korte hbe
+
+console.log("hello world");
+//28 min teke soro hbe
+console.log("we love Javascript");
+
+console.log("we are learning Javascript");
+
+
+/*npm init --y
+ata package.json file toiri korbe*/
+
+/*npm install nodemon -D
+ata node module file toiri korbe*/
+
+//development stage,production stage,staging stage
+
+//package.json e gie scripter oikane likbo-----> "start-dev" : "nodemon mdl1_coceptual1_faisalVai.js"
+//then
+//npm run start-dev
+
+console.log("I fell nJoy learning Javascript");
+console.log("I love Javascript");
+
+let x = 6;
+let y = 88;
+let z = x + y;
+let z2 = x * y;
+let z3 = x / y;
+console.log(z);
+console.log(z2);
+console.log(z3);
+
+//1:02 min teke soro hbe
+
+//.gitignore die 1t file banabo
+//git remote -v
+
+let name1 = "Sohel";
+//console.log(name);
+//console.log(name);
+let a = 5;
+console.log(a);
+console.log(name1);
+console.log("i love bangladesh");
+console.log("I am Sohel Shahid");
+
+
+
+
+
+
+
+
+
